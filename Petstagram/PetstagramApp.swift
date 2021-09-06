e1275ac17274f1e9338a9b707fffc16e0560947a
Vladimir Fibe <motiw@me.com>
2021-09-06 14:34:16 +0600

    Initial Commit

---
+++ Petstagram/PetstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetstagramApp.swift
+//  Petstagram
+//
+//  Created by Владимир on 06.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct PetstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
